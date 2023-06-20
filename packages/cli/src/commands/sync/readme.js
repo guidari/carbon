af2b87efc9167e6ffb600ee2cf06e72def5126b2
guidari <guilhermedatilio@gmail.com>
2023-06-20 09:36:01 -0300

    feat: added new hook useId for react 18 support

--- packages/cli/src/commands/sync/readme.js
+++ packages/cli/src/commands/sync/readme.js
@@ -18,8 +18,6 @@ const packageDenyList = new Set([
   'carbon-components',
   'carbon-components-react',
   '@carbon/react',
-  '@carbon/sketch',
-  '@carbon/styles',
 ]);
 
 function run({ root, packagePaths }) {
